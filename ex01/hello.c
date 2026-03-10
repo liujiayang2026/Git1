c061228da5c5e8fec95b3d8cf098797f4499521f
liujiayang2026 <Desolate2026@outlook.com>
2026-03-10 13:33:34 +0800

    添加了文字输入功能

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    printf("Hello World\n");
+void input() {
+    char str[100];
+    
+    printf("请输入内容：");
+    scanf("%s", str); 
+    
+    printf("您输入的是：%s\n", str);
+}
+
+int main()
+{
+    printf("Hello,world!\n");
+    input();
     return 0;
 }
